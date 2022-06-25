b7bc97aa0360b76f63424c4d53c66439a25a2580
Xie Zhihao <xzh1206@gmail.com>
2022-06-25 22:24:46 +0800

    Add YouTube MV provider

---
+++ server/models/mv/youtube/entry.js
@@ -0,0 +1,40 @@
+const NAME = "youtube";
+
+class Entry {
+  videoId;
+  title_;
+  channelTitle;
+
+  constructor(videoId, title, channelTitle) {
+    this.videoId = videoId;
+    this.title_ = title;
+    this.channelTitle = channelTitle;
+  }
+
+  id() {
+    return `${this.source()}.${this.videoId}`;
+  }
+
+  title() {
+    return this.title_;
+  }
+
+  subtitle() {
+    return "";
+  }
+
+  uploader() {
+    return this.channelTitle;
+  }
+
+  source() {
+    return NAME;
+  }
+
+  url() {
+    return `https://www.youtube.com/watch?v=${this.videoId}`;
+  }
+}
+
+export { NAME };
+export default Entry;
---
+++ server/models/mv/youtube/provider.js
@@ -0,0 +1,47 @@
+import Entry, { NAME } from "./entry";
+
+class Provider {
+  key;
+
+  name() {
+    return NAME;
+  }
+
+  configure(config) {
+    this.key = config.key;
+  }
+
+  async search(title) {
+    const res = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${title}&key=${this.key}`
+    );
+    const json = await res.json();
+
+    let result = [];
+    const items = json["items"];
+    for (const item of items) {
+      const snippet = item["snippet"];
+      result.push(
+        new Entry(
+          item["id"]["videoId"],
+          snippet["title"],
+          snippet["channelTitle"]
+        )
+      );
+    }
+    return result;
+  }
+
+  async get(id) {
+    const videoId = id.split(".")[1];
+    const res = await fetch(
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${this.key}`
+    );
+    const json = await res.json();
+
+    const snippet = json["items"][0]["snippet"];
+    return new Entry(videoId, snippet["title"], snippet["channelTitle"]);
+  }
+}
+
+export default Provider;
