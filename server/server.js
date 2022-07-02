707d420eb096aba5622a266d3c0b0d1416f2bcc1
Xie Zhihao <xzh1206@gmail.com>
2022-07-02 18:08:00 +0800

    Encode on demand

--- server/server.js
+++ server/server.js
@@ -42,8 +42,8 @@ class Server {
     this.downloader = new Downloader(
       downloadConfig["location"] ?? "./cache",
       downloadConfig["yt-dlp"] ?? "yt-dlp",
-      (entry, encode) => {
-        this.handleDownloadComplete(entry, encode);
+      (entry) => {
+        this.handleDownloadComplete(entry);
       }
     );
 
@@ -230,12 +230,7 @@ class Server {
     );
   }
 
-  handleDownloadComplete(entry, encode) {
-    if (!encode) {
-      this.handleEncodeComplete(entry);
-      return;
-    }
-
+  handleDownloadComplete(entry) {
     this.encoder.add(entry);
   }
 
--- server/utils/encode/encoder.js
+++ server/utils/encode/encoder.js
@@ -52,83 +52,96 @@ class Encoder {
     }
     let entry = this.list_[i];
 
-    // Encode
+    // Encode.
     this.encoding = true;
-    entry.onEncode();
     const mvPath = entry.mvPath();
-    switch (this.method) {
-      case Encoding.FFmpeg:
-        {
-          const karaokePath = `${mvPath}.aac`;
-          const genMVPath = `${mvPath}.gen.mp4`;
-          try {
-            await Utils.exec(
-              `${this.ffmpegLocation} -i ${mvPath} -map 0:a:0 -af pan="stereo|c0=c0|c1=-1*c1" -ac 1 -y ${karaokePath}`
-            );
-            await Utils.exec(
-              `${this.ffmpegLocation} -i ${mvPath} -i ${karaokePath} -map 0:v -map 0:a:0 -map 1 -y ${genMVPath}`
-            );
-            fs.rmSync(karaokePath);
-            fs.rmSync(mvPath);
-            fs.renameSync(genMVPath, mvPath);
-          } catch (e) {
-            console.error(e);
-            // Clean up
-            if (fs.existsSync(karaokePath)) {
-              fs.rmSync(karaokePath);
-            }
-            if (fs.existsSync(genMVPath)) {
-              fs.rmSync(genMVPath);
-            }
 
-            entry.onFail(e.message);
-            this.encoding = false;
-            this.encode();
-            return;
+    // Check count of audio tracks in advance.
+    let tracks = 2;
+    try {
+      await Utils.exec(`${this.ffmpegLocation} -i ${mvPath}`);
+    } catch (e) {
+      tracks = e.stderr
+        .split(/[\r\n]+/)
+        .filter((value) => value.match(/Stream #.*Audio/)).length;
+    }
+
+    if (tracks < 2) {
+      entry.onEncode();
+      switch (this.method) {
+        case Encoding.FFmpeg:
+          {
+            const karaokePath = `${mvPath}.aac`;
+            const genMVPath = `${mvPath}.gen.mp4`;
+            try {
+              await Utils.exec(
+                `${this.ffmpegLocation} -i ${mvPath} -map 0:a:0 -af pan="stereo|c0=c0|c1=-1*c1" -ac 1 -y ${karaokePath}`
+              );
+              await Utils.exec(
+                `${this.ffmpegLocation} -i ${mvPath} -i ${karaokePath} -map 0:v -map 0:a:0 -map 1 -y ${genMVPath}`
+              );
+              fs.rmSync(karaokePath);
+              fs.rmSync(mvPath);
+              fs.renameSync(genMVPath, mvPath);
+            } catch (e) {
+              console.error(e);
+              // Clean up.
+              if (fs.existsSync(karaokePath)) {
+                fs.rmSync(karaokePath);
+              }
+              if (fs.existsSync(genMVPath)) {
+                fs.rmSync(genMVPath);
+              }
+
+              entry.onFail(e.message);
+              this.encoding = false;
+              this.encode();
+              return;
+            }
           }
-        }
-        break;
-      case Encoding.SoX:
-        {
-          const musicPath = `${mvPath}.mp3`;
-          const karaokePath = `${mvPath}.k.mp3`;
-          const genMVPath = `${mvPath}.gen.mp4`;
-          try {
-            await Utils.exec(
-              `${this.ffmpegLocation} -i ${mvPath} -map 0:a:0 -y ${musicPath}`
-            );
-            await Utils.exec(
-              `${this.soxLocation} ${musicPath} ${karaokePath} oops`
-            );
-            fs.rmSync(musicPath);
-            await Utils.exec(
-              `${this.ffmpegLocation} -i ${mvPath} -i ${karaokePath} -map 0:v -map 0:a:0 -map 1 -y ${genMVPath}`
-            );
-            fs.rmSync(karaokePath);
-            fs.rmSync(mvPath);
-            fs.renameSync(genMVPath, mvPath);
-          } catch (e) {
-            console.error(e);
-            // Clean up
-            if (fs.existsSync(musicPath)) {
+          break;
+        case Encoding.SoX:
+          {
+            const musicPath = `${mvPath}.mp3`;
+            const karaokePath = `${mvPath}.k.mp3`;
+            const genMVPath = `${mvPath}.gen.mp4`;
+            try {
+              await Utils.exec(
+                `${this.ffmpegLocation} -i ${mvPath} -map 0:a:0 -y ${musicPath}`
+              );
+              await Utils.exec(
+                `${this.soxLocation} ${musicPath} ${karaokePath} oops`
+              );
               fs.rmSync(musicPath);
-            }
-            if (fs.existsSync(karaokePath)) {
+              await Utils.exec(
+                `${this.ffmpegLocation} -i ${mvPath} -i ${karaokePath} -map 0:v -map 0:a:0 -map 1 -y ${genMVPath}`
+              );
               fs.rmSync(karaokePath);
-            }
-            if (fs.existsSync(genMVPath)) {
-              fs.rmSync(genMVPath);
-            }
+              fs.rmSync(mvPath);
+              fs.renameSync(genMVPath, mvPath);
+            } catch (e) {
+              console.error(e);
+              // Clean up.
+              if (fs.existsSync(musicPath)) {
+                fs.rmSync(musicPath);
+              }
+              if (fs.existsSync(karaokePath)) {
+                fs.rmSync(karaokePath);
+              }
+              if (fs.existsSync(genMVPath)) {
+                fs.rmSync(genMVPath);
+              }
 
-            entry.onFail(e.message);
-            this.encoding = false;
-            this.encode();
-            return;
+              entry.onFail(e.message);
+              this.encoding = false;
+              this.encode();
+              return;
+            }
           }
-        }
-        break;
-      default:
-        throw new Error(`unexpected encoding "${this.method}"`);
+          break;
+        default:
+          throw new Error(`unexpected encoding "${this.method}"`);
+      }
     }
 
     this.completeCallback(entry);
