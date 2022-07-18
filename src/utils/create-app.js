b4dfd1396aee17eb42a0eb44ba9fc42886705cc1
maoqxxmm <maoqxm@gmail.com>
2022-07-18 22:21:13 +0800

    Extract common logic of applications

---
+++ src/utils/create-app.js
@@ -0,0 +1,27 @@
+import { ConfigProvider } from "antd";
+import i18n from "i18next";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { initReactI18next } from "react-i18next";
+import zh from "../locales/zh";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    zh: {
+      ...zh,
+    },
+  },
+  lng: "zh",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export const createApp = (appComp) => {
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <ConfigProvider autoInsertSpaceInButton={false}>{appComp}</ConfigProvider>
+    </React.StrictMode>
+  );
+};
