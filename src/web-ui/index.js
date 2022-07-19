be3295a0ce72e083978c74f1b65dffca5942bdb9
毛球 <maoqxm@gmail.com>
2022-07-19 10:21:01 +0800

    Extract common logic of applications (#27)

--- src/web-ui/index.js
+++ src/web-ui/index.js
@@ -1,28 +1,4 @@
-import { ConfigProvider } from "antd";
-import i18n from "i18next";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { initReactI18next } from "react-i18next";
+import { createApp } from "../utils";
 import App from "./App";
-import zh from "../locales/zh";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    zh: {
-      ...zh,
-    },
-  },
-  lng: "zh",
-  interpolation: {
-    escapeValue: false,
-  },
-});
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <ConfigProvider autoInsertSpaceInButton={false}>
-      <App />
-    </ConfigProvider>
-  </React.StrictMode>
-);
+createApp(<App />);
