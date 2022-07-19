83719d45eca564f934f298e683cd3c22c25e142f
Xie Zhihao <xzh1206@gmail.com>
2022-07-19 10:39:59 +0800

    Re-export src components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as DrawerWidget } from "./DrawerWidget";
+export { default as PopoverWidget } from "./PopoverWidget";
+
+export { default as PlayControlWindow } from "./PlayControlWindow";
+export { default as PlaylistWindow } from "./PlaylistWindow";
+export { default as SearchWindow } from "./SearchWindow";
---
+++ src/locales/index.js
@@ -0,0 +1,1 @@
+export { default as zh } from "./zh";
